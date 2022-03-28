74509098e23be8389e4b8cb60d9474bc6c93851e
Ameya1301 <92557148+Ameya1301@users.noreply.github.com>
2022-03-28 15:58:19 +0530

    Add files via upload

--- src/rooms/tictactoe.ts
+++ src/rooms/tictactoe.ts
@@ -1,11 +1,14 @@
 import {Room, Client} from "colyseus";
 import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
+var j =0;
 
 
 class Client2 extends Schema {
     client:Client;
-    client2(client){
+    myTurn:boolean;
+    Client2(client){
         this.client = client;
+        this.myTurn = false;
     }
 }
 
@@ -17,11 +20,11 @@ class Client2 extends Schema {
 // }
 
 class Turn extends Schema {
-    sessionId: string;
+    // sessionId: string;
     position: number;
 
-    Turn(sessionId,position){
-        this.sessionId= sessionId;
+    Turn( position){
+        // this.sessionId = sessionId;
         this.position = position;
     }
 }
@@ -30,9 +33,11 @@ export class Game extends Schema {
     @type([ "number" ]) gameArr = new ArraySchema<number>();
     // @type("string")  turn;
     // userMap:Map<string,Client>;
+    //session ID - client obj
     @type({ map: Client2 }) userMap = new MapSchema<Client2>();
     @type({ map: Turn }) turnMap = new MapSchema<Turn>();
     // @type(CurrentTurn) turn:CurrentTurn = new CurrentTurn();
+     @type("string") CurrentTurn = "";
     
     
     Game(){
@@ -41,8 +46,6 @@ export class Game extends Schema {
             this.gameArr.push(-1);
         }   
     }  
-    
-
     
 
     createPlayer(client: Client){
@@ -69,10 +72,26 @@ export class Game extends Schema {
     // }
     
     startGame(maxClients: number){
+        if(maxClients === this.userMap.size)
+        {   
+            this.CurrentTurn = this.userMap.keys().next().value;
+           
+            
+            // this.turnMap.clear();
+           
+            
+
+           
+
+        }
+
+   
         
+        //set turn session id
+
+        // 
+
         // console.log(this.turn);
-        // if(maxClients === this.userMap.size)
-        // {
         //     console.log("game can start");
 
             // this.turn.turn = this.userMap.keys[0];
@@ -85,6 +104,13 @@ export class Game extends Schema {
             //     this.turn.turn = this.userMap.keys[1];
             
             
+    }
+    
+   
+
+
+    playTurn(){
+        
     }
           
         // var i=0;
@@ -110,8 +136,24 @@ export class Tictactoe extends Room {
         console.log("Room Created");
         this.setState(new Game());
         
-        this.onMessage("position", (client, message) => {
+        this.onMessage("move", (client, message) => {
             console.log("from ", client.sessionId , " received: ", message);
+           
+            console.log(this.state.turnMap.forEach((value, key)=> {
+                console.log(value.position);
+                console.log(key);
+            }));
+
+            console.log("j: ",j);
+            // this.state.turnMap.set(j , new Turn(message));
+            this.state.turnMap[j] = new Turn(message);
+        
+            j++;
+
+            console.log(this.state.turnMap.get(j-1));
+
+            
+            
         });
     }
 
@@ -121,7 +163,7 @@ export class Tictactoe extends Room {
 
     onJoin (client: Client) {
         
-        client.send("joined Successfully");
+        client.send("joined Successfully",client);
         console.log(client.sessionId, ' has joined');
         this.state.createPlayer(client);
         this.state.startGame(this.maxClients);
